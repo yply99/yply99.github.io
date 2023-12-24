56b96b560752d37c2499f3d367081b4e1f925876
Just A Cat <154723473+yply99@users.noreply.github.com>
2023-12-23 23:00:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
     var password = prompt("Please enter the password to view this album:", "");
-    if (password == "yourPassword") {
+    if (password == "yp61520") {
         document.getElementById('content').style.display = 'block';
     } else {
         document.body.innerHTML = "<h2>Incorrect password. Access denied.</h2>";
